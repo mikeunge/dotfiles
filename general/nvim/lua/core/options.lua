c4f179bd15c3b8aa3c7873b43e492a6a1af592d8
Mike Unge <mikeunge@protonmail.com>
2021-10-19 16:04:58 +0200

    chg: nvim is now set to nowrap (lines), polybar bg color chagned and a new module was added

--- general/nvim/lua/core/options.lua
+++ general/nvim/lua/core/options.lua
@@ -6,6 +6,9 @@ g.nvchad_user_config = "chadrc"
 
 local options = require("core.utils").load_config().options
 
+-- set nowrap
+vim.wo.wrap = false
+
 opt.title = true
 opt.clipboard = options.clipboard
 opt.cmdheight = options.cmdheight
