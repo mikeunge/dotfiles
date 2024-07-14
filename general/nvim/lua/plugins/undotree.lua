a30642878289827f72735d98c2bf20bfd972f9ab
Mike <18002494+mikeunge@users.noreply.github.com>
2024-07-14 13:40:08 +0200

    chore: nuke everything

--- general/nvim/lua/plugins/undotree.lua
+++
@@ -1,7 +0,0 @@
-local target_path = vim.fn.expand('~/.undodir')
-
-if vim.fn.isdirectory(target_path) == false then
-    vim.call(vim.fn.mkdir(target_path, "p", 0700))
-end
-
-vim.cmd("set undodir", target_path)
